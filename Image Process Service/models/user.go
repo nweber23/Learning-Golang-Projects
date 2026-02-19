f7c827bd386fc3cbdf1af6b5f752d1bf82edf04c
Niklas Weber <nweber@student.42Heilbronn.de>
2026-02-19 17:05:29 +0100

    feat: add user model and authentication request/response structures

---
+++ Image Process Service/models/user.go
@@ -0,0 +1,26 @@
+package models
+
+import "time"
+
+type User struct {
+	ID           string    `json:"id"`
+	Username     string    `json:"username"`
+	PasswordHash string    `json:"-"`
+	CreatedAt    time.Time `json:"created_at"`
+}
+
+type RegisterRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type LoginRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type AuthResponse struct {
+	ID       string `json:"id"`
+	Username string `json:"username"`
+	JWT      string `json:"jwt"`
+}
