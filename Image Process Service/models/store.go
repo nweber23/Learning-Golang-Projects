957c92aa2c60121bfd3747da32958f64e72e4354
Niklas Weber <nweber@student.42Heilbronn.de>
2026-02-22 13:16:36 +0100

    feat: implement user and image management methods in Store

--- Image Process Service/models/store.go
+++ Image Process Service/models/store.go
@@ -23,6 +23,8 @@ func (s *Store) SaveUser(user *User) error {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
+	s.users[user.ID] = user
+	s.usersByUsername[user.Username] = user.ID
 	return nil
 }
 
@@ -30,20 +32,29 @@ func (s *Store) FindUserByUsername(username string) (*User, error) {
 	s.mu.RLock()
 	defer s.mu.RUnlock()
 
-	return nil, nil
+	userID, ok := s.usersByUsername[username]
+	if !ok {
+		return nil, nil
+	}
+	return s.users[userID], nil
 }
 
 func (s *Store) FindUserByID(id string) (*User, error) {
 	s.mu.RLock()
 	defer s.mu.RUnlock()
 
-	return nil, nil
+	user, ok := s.users[id]
+	if !ok {
+		return nil, nil
+	}
+	return user, nil
 }
 
 func (s *Store) SaveImage(image *Image) error {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
+	s.images[image.ID] = image
 	return nil
 }
 
@@ -51,20 +62,43 @@ func (s *Store) FindImageByID(id string) (*Image, error) {
 	s.mu.RLock()
 	defer s.mu.RUnlock()
 
-	return nil, nil
+	image, ok := s.images[id]
+	if !ok {
+		return nil, nil
+	}
+	return image, nil
 }
 
 func (s *Store) ListImagesByUser(userID string, page, limit int) ([]*Image, int, error) {
 	s.mu.RLock()
 	defer s.mu.RUnlock()
 
-	return nil, 0, nil
+	var userImages []*Image
+	for _, img := range s.images {
+		if img.UserID == userID {
+			userImages = append(userImages, img)
+		}
+	}
+
+	total := len(userImages)
+	start := (page - 1) * limit
+	end := start + limit
+
+	if start > total {
+		return []*Image{}, total, nil
+	}
+	if end > total {
+		end = total
+	}
+
+	return userImages[start:end], total, nil
 }
 
 func (s *Store) DeleteImage(id string) error {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
+	delete(s.images, id)
 	return nil
 }
 
@@ -72,6 +106,7 @@ func (s *Store) SaveTransformation(transformation *Transformation) error {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
+	s.transformations[transformation.ID] = transformation
 	return nil
 }
 
@@ -79,5 +114,9 @@ func (s *Store) FindTransformationByID(id string) (*Transformation, error) {
 	s.mu.RLock()
 	defer s.mu.RUnlock()
 
-	return nil, nil
+	transformation, ok := s.transformations[id]
+	if !ok {
+		return nil, nil
+	}
+	return transformation, nil
 }
